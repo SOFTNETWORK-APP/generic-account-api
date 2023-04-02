e6a352e3da31faa43a7cf4471db9f4ae34578bd1
Stéphane Manciot <stephane.manciot@gmail.com>
2023-04-02 10:44:13 +0200

    update dependencies

--- project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
+++ project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
@@ -4,13 +4,13 @@ object Versions {
 
   val genericPersistence = "0.2.6.1"
 
-  val notification = "0.2.3"
+  val notification = "0.2.4"
 
-  val scheduler = "0.2.4"
+  val scheduler = "0.2.5.3"
 
   val server = "0.2.8"
 
-  val session = "0.1.2"
+  val session = "0.1.3"
 
   val scalatest = "3.1.1"
 }
