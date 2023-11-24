d9b0a8effbd255a4ead18eeaa90f33a90b37a1cc
Stéphane Manciot <stephane.manciot@gmail.com>
2023-11-24 18:29:06 +0100

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
-  val genericPersistence = "0.5.0"
+  val genericPersistence = "0.5.1"
 
-  val notification = "0.5.0"
+  val notification = "0.5.1"
 
-  val scheduler = "0.5.0"
+  val scheduler = "0.5.1"
 
   val scalatest = "3.2.16"
 
