7c258953ce8bd22aa4f43c590c1ba3ba17f3ac88
Stéphane Manciot <stephane.manciot@gmail.com>
2023-12-05 13:57:12 +0100

    upgrade persistence, scheduler and notification dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
-  val genericPersistence = "0.6.0"
+  val genericPersistence = "0.6.1"
 
-  val notification = "0.6.1"
+  val notification = "0.6.2"
 
-  val scheduler = "0.6.1"
+  val scheduler = "0.6.2"
 
   val scalatest = "3.2.16"
 
